e5cf33f6e48a9153e5e4b5c4a65d7366724be4ac
anatoly burtsev <anatolyburtsev@yandex.ru>
2014-12-23 21:13:35 +0300

    add port for redis

--- redis_backup.bash
+++ redis_backup.bash
@@ -3,10 +3,11 @@ set -e
 
 backup_server="deposito"
 path_to_remote_backup="antivirus/redis" 
+port="6979"
 
-data=`redis-cli CONFIG get dir|tail -1`
+data=`redis-cli -p $port CONFIG get dir|tail -1`
 rm -f $data/dump.rdb 2>/dev/null || :
-res=`redis-cli SAVE`
+res=`redis-cli -p $port SAVE`
 if [ "$res" == "OK" ]; then
     rsync $data/dump.rdb ${backup_server}::${path_to_remote_backup}/`hostname -s``date +%Y%m%d%H%M`.rdb
 fi

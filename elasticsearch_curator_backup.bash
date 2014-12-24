a71748ec1c11dfd0a94efca2dc9a2e359e2799e9
anatoly burtsev <anatolyburtsev@yandex.ru>
2014-12-24 14:20:24 +0300

    Update elasticsearch_curator_backup.bash

--- elasticsearch_curator_backup.bash
+++ elasticsearch_curator_backup.bash
@@ -2,7 +2,8 @@
 set -e
 backup_server="deposito"
 path_to_remote_backup="antivirus/elasticsearch"
+curator=`which curator`
 
-curator --loglevel ERROR snapshot --repository my_backup --delete-older-than 1
-curator --loglevel ERROR snapshot --all-indices --repository my_backup
+$curator --loglevel ERROR snapshot --repository my_backup --delete-older-than 1
+$curator --loglevel ERROR snapshot --all-indices --repository my_backup
 rsync -ra /place/elasticsearch/backup ${backup_server}::${path_to_remote_backup}/`hostname -s``date +%Y%m%d%H%M`

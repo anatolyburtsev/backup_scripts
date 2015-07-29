f666d7b3dfbecfb3a80c940209b78244399b063b
anatoly burtsev <anatolyburtsev@yandex.ru>
2015-07-29 13:37:35 +0300

    remove trash

--- elasticsearch_curator_backup.bash
+++ elasticsearch_curator_backup.bash
@@ -2,28 +2,10 @@
 set -eu
 backup_server="deposito.yandex.ru"
 path_to_remote_backup="antivirus/elasticsearch"
-<<<<<<< HEAD
-curator=/antivirus/.env/bin/curator #`which curator`
 
-#setup snapshot for elasticsearch 
-res=`curl -XGET 'http://localhost:9200/_snapshot' 2>/dev/null`
-
-if [ "$res" == "{}" ] ; then 
-
-curl -XPUT 'http://localhost:9200/_snapshot/my_backup' -d '{
-    "type": "fs",
-    "settings": {
-        "location": "/place/elasticsearch/backup",
-        "compress": true
-    }
-}'
-
-fi
-=======
 curator=`which curator`
 #setup snapshot for elasticsearch 
 res=`curl -XGET 'http://localhost:9200/_snapshot' 2>/dev/null`
->>>>>>> e65090a9999b69d79b458606345a5839ac929d90
 
 if [ "$res" == "{}" ] ; then 
 
